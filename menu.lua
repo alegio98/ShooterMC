8018e38dd093020dc2384c3dc08a5dfc91ec446f
alegio98 <61783866+alegio98@users.noreply.github.com>
2020-07-13 12:44:56 +0200

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -2,11 +2,6 @@ local composer = require( "composer" )
 
 local scene = composer.newScene()
 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
-
 local function gotoGame()
     composer.gotoScene( "game", { time=800, effect="crossFade" } )
 end
